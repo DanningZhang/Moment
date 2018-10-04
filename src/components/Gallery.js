19edca13f954412113d869954fdb34c9ef9c29db
DanningZhang <danningzhang11@gmail.com>
2018-10-03 22:57:30 -0500

    GeoLocation

---
+++ src/components/Gallery.js
@@ -0,0 +1,13 @@
+import React, { Component }from 'react';
+import PropTypes from 'prop-types';
+import GridGallery from 'react-grid-gallery';
+
+export class Gallery extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Tabs, Button, Spin } from 'antd';
+import {GEO_OPTIONS, POS_KEY} from "../constants";
+
+const TabPane = Tabs.TabPane;
+
+const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    state = {
+        loadingGeoLocation: false,
+        loadingPosts: false,
+        error: '',
+        posts: [],
+    }
+
+    componentDidMount() {
+        this.setState({ loadingGeoLocation: true, error: '' });
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeolocation,
+                GEO_OPTIONS
+            );
+        } else {
+            this.setState({error : 'Your browser does not support geolocation!'})
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+        const {latitude, longitude} = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({latitude, longitude}));
+        this.setState({loadingGeoLocation : false, error : ''});
+    }
+
+    onFailedLoadGeolocation = (error) => {
+        console.log(error);
+        this.setState({loadingGeoLocation: false, error: 'Failed to load geo location!'});
+    }
+
+    getGalleryPanelContent = () => {
+        if (this.state.error) {
+            return <div>{this.state.error}</div>
+        } else if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading geolocation..."/>
+        } else {
+            return null;
+        }
+    }
+
     render() {
         return (
-            <div>this is home</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Post" key="1">
+                    {this.getGalleryPanelContent()}
+                </TabPane>
+                <TabPane tab="Map 2" key="2">Content of tab 2</TabPane>
+            </Tabs>
         );
     }
 }
